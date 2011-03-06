bc44933f364794dc122361a2ac6071a1b990d846
Praveen Selvam <praveen.selvam@gmail.com>
2011-03-06 18:40:43 +0530

    #ui [Praveen] - Added web fonts. Fixed report child display in IE7.

--- children-hugs/shared/includes.php
+++ children-hugs/shared/includes.php
@@ -18,6 +18,8 @@
 
 <link rel="stylesheet" href="./css/jquery/jquery-ui-1.8.10.custom.css" type="text/css" media="screen, projection">
 
+<link rel="stylesheet" href="./css/web-fonts.css" type="text/css" media="screen, projection">
+
 <!-- App CSS -->
 <link rel="stylesheet" href="./css/main.css" type="text/css" media="screen, print">
 <link rel="stylesheet" href="./css/search_child_widget.css" type="text/css" media="screen, print">
